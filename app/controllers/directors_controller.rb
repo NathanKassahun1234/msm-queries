f54dce3146410c6706b523d2f7afdea27e44d086
NathanKassahun1234 <Kassahunny@uchicago.edu>
2024-11-05 05:48:49 +0000

    Adds directors#show

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,16 @@
 class DirectorsController < ApplicationController 
-  def index 
-    @directors = Director.all
-    render ({ :template => "director_templates/list" })
-  end 
-end 
+  def index
+    render({ :template => "director_templates/list" })
+  end
+
+  def show
+    @the_director = Director.find_by(id: params[:id]) # Use `find_by` to return nil if not found
+
+    if @the_director.nil?
+      # Handle the case where the director is not found
+      redirect_to directors_path, alert: "Director not found"
+    else
+      render({ template: "director_templates/details" })
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
   get("/", { :controller => "misc", :action => "homepage" })
-  get("/directors", { :controller => "directors", :action => "index"})
+  resources :directors, only: [:index, :show]
+  
 end
