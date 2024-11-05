4283de18483e210fa27584a14b1dd5b03a233db1
NathanKassahun1234 <Kassahunny@uchicago.edu>
2024-11-05 06:19:58 +0000

    50 percent

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   resources :directors, only: [:index, :show]
   
+  get("/movies", { :controller => "movies", :action => "index"})
+  get("/movies/:the_id", { :controller => "movies", :action => "show"})
 end
